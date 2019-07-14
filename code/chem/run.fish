312576b39e14dd9c5378715893ce8fa82dbaca04
S Mautner <mautner@cs.uni-freiburg.de>
2019-07-14 10:08:44 +0200

    cpu-

--- code/chem/run.fish
+++ code/chem/run.fish
@@ -12,19 +12,20 @@ for model in $models
     set -e trainadd 
     set -e sampleadd 
     if test $model = 'organ'; set trainadd '--pg_iters' 10; end  # 10 is still ok just 2 for my test, neee
-    mkdir $data/$model
+    mkdir -p $data/$model
     python scripts/train.py $model --train_load $data/train.csv\
                                    --model_save  $data/$model/mod\
                                    --vocab_save $data/$model/voc\
                                    --n_jobs 40\
-                                   --device cpu\
+                                   --device cuda\
                                    --config_save $data/$model/conf $trainadd
 
     python scripts/sample.py $model --model_load $data/$model/mod\
                                     --vocab_load $data/$model/voc\
                                     --n_samples 5000\
                                     --gen_save $data/$model/gen\
-                                    --device cpu\
+                                    --device cuda\
                                     --config_load $data/$model/conf $sampleadd
+
 end
 
