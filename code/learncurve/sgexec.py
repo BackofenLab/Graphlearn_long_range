1cbaef7b01937cae4dd0ad98d9f6bb36230774c0
S Mautner <mautner@cs.uni-freiburg.de>
2019-12-17 16:52:56 +0100

    jobqueclass ok

--- code/learncurve/sgexec.py
+++ code/learncurve/sgexec.py
@@ -5,22 +5,22 @@ import pickle
 
 
 
-def collectresult(tid,numres):
-    load = lambda x: pickle.load( open( "res/%d.pickle" % x, "rb" ) )  
-    return [load(i+1) for i in range(numres)]
+def collectresults(jobid,numtasks):
+    load = lambda x: pickle.load( open( "res/%d_%d.pickle" % (jobid,x), "rb" ))
+    return [load(i+1) for i in range(numtasks)]
 
 def sgexec(func,iterab): 
     # to file
-    pickle.dump( (func,iterab), open( "tasks.pickle", "wb" ) )
+    pickle.dump( (func,iterab), open( "tasks0.pickle", "wb" ) )
     # qsub
-    ret,stderr,out = ba.shexec("qsub -V -t 1-%d  sgexec.sh 1"  % len(iterab))
+    ret,stderr,out = ba.shexec("qsub -V -t 1-%d  sgexec.sh 0"  % len(iterab))
     taskid =  out.split()[2][:7]
     print ('taskid:',taskid)
     time.sleep(2)
     while taskid in ba.shexec("qstat")[2]:
         time.sleep(5)
 
-    return collectresult(len(iterab))
+    return collectresults(0,len(iterab))
 
 
 class sgeexecuter: 
@@ -36,28 +36,27 @@ class sgeexecuter:
     def execute(self):
         # start jobs 
         task_ids =  []
-        for i,size in enumerate(self.lenlist):
-            ret,stderr,out = ba.shexec("qsub -V -t 1-%d  sgexec.sh %d"  %(size,i)) 
+        for jobid,size in enumerate(self.lenlist):
+            ret,stderr,out = ba.shexec("qsub -V -t 1-%d  sgexec.sh %d"  %(size,jobid)) 
             taskid =  out.split()[2][:7]
             task_ids.append(taskid)
+            time.sleep(2)
         while True:
            time.sleep(5)
            qstat = ba.shexec("qstat")[2]
-           if not any([tid in qstat for tid in task_id ]):
+           if not any([tid in qstat for tid in task_ids ]):
                break
 
-
-
-        results = [ collectresults(i,e) for i,e in enumerate(self.lenlist) ]
+        results = [ collectresults(jobid,numtasks) for jobid,numtasks in enumerate(self.lenlist) ]
         self.lenlist=[]
         return results
 
 
 if __name__=="__main__":
     import sys
-    task_id = int(sys.argv[1])
-    job_id = int(sys.argv[2])
-    func,iterab = pickle.load( open( "tasks.pickle", "rb" ) )
+    job_id = int(sys.argv[1])
+    task_id = int(sys.argv[2])
+    func,iterab = pickle.load( open( "tasks%d.pickle" % job_id, "rb" ) )
     res = func(iterab[task_id-1])
-    pickle.dump( res , open( "res/%d%d.pickle" % (task_id,job_id) , "wb" ) )
+    pickle.dump( res , open( "res/%d_%d.pickle" % (job_id,task_id) , "wb" ) )
 
--- code/learncurve/sgexec.sh
+++ code/learncurve/sgexec.sh
@@ -7,6 +7,6 @@
 #$ -R y
 #$ -o /home/mautner/JOBZ/reconstr_o/$JOB_ID.o_$TASK_ID
 #$ -e /home/mautner/JOBZ/reconstr_e/$JOB_ID.e_$TASK_ID
-python sgexec.py $SGE_TASK_ID $1
+python sgexec.py $1 $SGE_TASK_ID 
 #qsub -V -t 1-900 sgexec.sh
 
