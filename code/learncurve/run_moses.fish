a7aa06e8c5f24578448ee940b679e434eac2531d
S Mautner <mautner@cs.uni-freiburg.de>
2020-01-15 02:45:57 +0100

    some params changed

--- code/learncurve/run_moses.fish
+++ code/learncurve/run_moses.fish
@@ -1,10 +1,12 @@
 set data data5000
 set path $HOME/repos/moses
+set grammars classic coarse priosim coarseloco
 
 switch $argv[1]
 
 case --help
     echo "look at the code"
+
 
 case debug
     python main2.py     --n_jobs 10\
@@ -16,19 +18,21 @@ case debug
                         --burnin 10\
                         --n_steps 11\
                         --emit 4
-
-
-case classic
-    mkdir -p $path/$data/classic/
-    python main2.py     --n_jobs 10\
-                        --no-sge\
-                        --train_load $path/$data/train.csv\
-                        --n_samples 1000\
-                        --gen_save $path/$data/classic/gen\
-                        --grammar classic\
-                        --burnin 10\
-                        --n_steps 11\
-                        --emit 4
-end 
-
+case runall 
+    for grammar in $grammars 
+        mkdir -p $path/$data/$grammar
+        python3 main2.py --n_jobs 8\
+                         --no-sge\
+                         --grammar $grammar\
+                         --train_load $path/$data/train.csv\
+                         --gen_save $path/$data/$grammar/gen\
+                         --n_samples 11\
+                         --testsize 500\
+                         --loglevel 99\
+                         --burnin 20\
+                         --emit 5\
+                         --n_steps 21\
+                         --repeatseeds 1333
+    end
+end
 
